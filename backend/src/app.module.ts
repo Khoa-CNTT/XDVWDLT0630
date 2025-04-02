24c441b66997fb179a9dfc41a81a6451a1b6458e
ninh2103 <nguyenducninh210303@gmail.com>
2025-04-02 13:05:39 +0700

    Initial commit with selected backend files

---
+++ backend/src/app.module.ts
@@ -0,0 +1,57 @@
+import { Module, ValidationPipe } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_PIPE } from '@nestjs/core';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { RoleModule } from './modules/role/role.module';
+import { UserModule } from './modules/user/user.module';
+import { BlogModule } from './modules/blog/blog.module';
+import { TagsModule } from './modules/tags/tags.module';
+import { VaccinationsModule } from './modules/vaccinations/vaccinations.module';
+import { SupplierModule } from './modules/supplier/supplier.module';
+import { ManufacturersModule } from './modules/manufacturers/manufacturers.module';
+import { InventoryModule } from './modules/inventory/inventory.module';
+import { BookingsModule } from './modules/bookings/bookings.module';
+import { MomoModule } from './modules/momo/momo.module';
+import { AppointmentsModule } from './modules/appointments/appointments.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { NotificationsModule } from './modules/notifications/notifications.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ScheduleModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/uploads',
+    }),
+    AuthModule,
+    UserModule,
+    RoleModule,
+    BlogModule,
+    TagsModule,
+    VaccinationsModule,
+    SupplierModule,
+    ManufacturersModule,
+    InventoryModule,
+    BookingsModule,
+    MomoModule,
+    AppointmentsModule,
+    NotificationsModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
+    // config de dung duoc HttpException
+  ],
+})
+export class AppModule {}
---
+++ backend/src/main.ts
@@ -0,0 +1,47 @@
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setGlobalPrefix('api');
+
+  // config swagger api
+  const config = new DocumentBuilder()
+    .setTitle('Vaccination')
+    .setDescription('The Vaccination API description')
+    .setVersion('1.0')
+    .addTag('auth')
+    .addTag('appointments')
+    .addTag('blog')
+    .addTag('bookings')
+    .addTag('user')
+    .addTag('inventory')
+    .addTag('manufacturers')
+    .addTag('momo')
+    .addTag('notifications')
+    .addTag('supplier')
+    .addTag('tag')
+    .addTag('role')
+    .addTag('vaccinations')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    customSiteTitle: 'Swagger | Vaccination',
+  });
+
+  // Configure port on Frontend access side
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  };
+
+  app.enableCors(corsOptions);
+  await app.listen(3001);
+}
+
+bootstrap();
